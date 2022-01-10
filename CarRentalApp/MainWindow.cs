b7ee6660ece13db3cbac7183d9ccb47b295ebea9
Ornella Fanais <ornella.fanais@avanade.com>
2022-01-10 16:34:51 +0100

    UI/UX tricks

--- CarRentalApp/MainWindow.cs
+++ CarRentalApp/MainWindow.cs
@@ -20,22 +20,39 @@ namespace CarRentalApp
         private void addRentalRecordToolStripMenuItem_Click(object sender, EventArgs e)
         {            
             var addRentalRecord = new AddEditRentalRecord();
+            //Simple way of allowing are ensuring that only one
+            //istance of the window open
+            addRentalRecord.ShowDialog();
             addRentalRecord.MdiParent = this;
-            addRentalRecord.Show();
         }
 
         private void manageVehicleListingToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var vehicleListing = new ManageVehicleListing();
-            vehicleListing.MdiParent = this;
-            vehicleListing.Show();
+            //give me a list od the open forms of all of the data type form
+            var OpenForms = Application.OpenForms.Cast<Form>();
+            //declare a variable where I want to say is open
+            var isOpen = OpenForms.Any(q => q.Name == "ManageVehicleListing");
+            if (!isOpen)
+            {
+                var vehicleListing = new ManageVehicleListing();
+                vehicleListing.MdiParent = this;
+                vehicleListing.Show();
+            }
+            
         }
 
         private void viewArchiveToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var manageRentalRecords = new ManageRentalRecords();
-            manageRentalRecords.MdiParent = this;
-            manageRentalRecords.Show();
+            var OpenForms = Application.OpenForms.Cast<Form>();
+            //declare a variable where I want to say is open
+            var isOpen = OpenForms.Any(q => q.Name == "ManageRentalRecords");
+            if (!isOpen)
+            {
+                var manageRentalRecords = new ManageRentalRecords();
+                manageRentalRecords.MdiParent = this;
+                manageRentalRecords.Show();
+            }
+                
         }
     }
 }
